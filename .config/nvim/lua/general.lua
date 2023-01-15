41dbcf903617bcac1c493ef24f750621fbe691a9
bkorecic <blaz.korecic@ug.uchile.cl>
2023-01-15 01:27:51 -0300

    Add nvim-cmp and snippy

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -1,3 +1,6 @@
+-- Clipboard
+vim.opt.clipboard = 'unnamedplus'
+
 -- Indentation
 vim.opt.autoindent = true
 vim.opt.smarttab = true
--- .config/nvim/lua/plugin-conf.lua
+++ .config/nvim/lua/plugin-conf.lua
@@ -1,1 +1,90 @@
+-- Use CCLS as C++ language server
 require('lspconfig').ccls.setup{}
+
+-- Lualine
+require('lualine').setup()
+
+-- Snippy
+local snippy = require('snippy')
+snippy.setup({
+    mappings = {
+        is = {
+            ['<Tab>'] = 'expand_or_advance',
+            ['<S-Tab>'] = 'previous',
+        },
+    },
+})
+
+-- nvim-cmp
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require('snippy').expand_snippet(args.body) -- For `snippy` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			--elseif snippy.can_expand_or_advance() then
+				--snippy.expand_or_advance()
+			--elseif has_words_before() then
+				--cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif snippy.can_jump(-1) then
+				snippy.previous()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['ccls'].setup {
+	capabilities = capabilities
+}
