b6e2e3b758a18a91c294e33966c755bde2bd5313
bkorecic <32780326+bkorecic@users.noreply.github.com>
2025-08-22 22:43:22 -0400

    add fuzzel + pnpm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,10 @@ end
 
 # Theme
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/bkorecic/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
